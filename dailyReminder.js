af4b99369c1eaa892c9d566d02e04a0759917097
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-03-04 11:48:30 -0600

    Adjusts spacing in message

--- dailyReminder.js
+++ dailyReminder.js
@@ -9,11 +9,10 @@ module.exports = (client) => {
         const channel = await client.channels.fetch(channelId);
         if (!channel)
           return console.error(`Channel with ID ${channelId} not found.`);
-
         await channel.send(
-          `**STATEMENT:** This is your reminder to send a stand-up report. /n
-					**QUALIFICATION** Your report should include a summary of your work yesterday and your plan for today. /n
-					**CAUTIONARY** Stand-ups are for you and GJT Industries. Do not stall in sending them.`
+          `**STATEMENT:** This is your reminder to send a stand-up report.\n\n` +
+            `**QUALIFICATION:** Your report should include a summary of your work yesterday and your plan for today.\n\n` +
+            `**CAUTIONARY:** Stand-ups are for you and GJT Industries. Do not stall in sending them.`
         );
         console.log("Daily reminder sent at 5pm (Mon-Fri)!");
       } catch (error) {
